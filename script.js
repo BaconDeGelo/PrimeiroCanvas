2efe679f842c169832715659176deec1e8bf70c0
BaconDeGelo <pedro.ugo2015@gmail.com>
2025-07-02 20:01:38 -0300

    Formas diferentes, Posição X e Y. fillStyle, stroke (1.0.3)

--- script.js
+++ script.js
@@ -38,7 +38,75 @@ ctx.arc(300, 200, 50, 0, Math.PI * 2);
 // Fecha o caminho (opcional para círculos completos, mas boa prática)
 ctx.closePath();
 
-// Preenche o círculo com a cor atual (vermelho).
+ctx.fill();
+
+ctx.beginPath();
+
+ctx.moveTo(150, 100);
+ctx.lineTo(100, 200);
+ctx.lineTo(200, 200);
+
+ctx.closePath();
+
+ctx.fillStyle = 'green';
+
+ctx.fill();
+
+ctx.fillStyle = 'purple';
+ctx.beginPath();
+ctx.moveTo(200, 10);
+ctx.lineTo(220, 30);
+ctx.lineTo(200, 50);
+ctx.lineTo(180, 30);
+ctx.closePath();
+ctx.fill();
+
+ctx.fillStyle = 'gold';
+ctx.beginPath();
+ctx.moveTo(260, 15);
+ctx.lineTo(265, 25);
+ctx.lineTo(275, 25);
+ctx.lineTo(268, 32);
+ctx.lineTo(270, 42);
+ctx.lineTo(260, 37);
+ctx.lineTo(250, 42);
+ctx.lineTo(252, 32);
+ctx.lineTo(245, 25);
+ctx.lineTo(255, 25);
+ctx.closePath();
+ctx.fill();
+
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 3;
+ctx.beginPath();
+ctx.arc(30, 100, 18, 0, Math.PI * 2)
+ctx.stroke();
+
+ctx.fillStyle = 'orange'
+ctx.fillRect(50, 200, 90, 50)
+
+ctx.beginPath();
+
+ctx.moveTo(300, 100);
+ctx.lineTo(200, 100);
+ctx.lineTo(235, 200);
+
+ctx.closePath();
+
+ctx.fillStyle = 'cyan';
+
+ctx.fill();
+
+ctx.fillStyle = 'pink';
+ctx.beginPath();
+ctx.moveTo(400, 50);
+ctx.lineTo(370, 90);
+ctx.lineTo(370, 124);
+ctx.lineTo(400, 164);
+ctx.lineTo(430, 124);
+ctx.lineTo(430, 90);
+ctx.lineTo(400, 50);
+ctx.closePath();
 ctx.fill();
 
 // --- Funcionalidade do Botão Limpar ---
